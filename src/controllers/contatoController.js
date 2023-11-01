ea8e6f4b19728f0a79fa8cca92c4b92c4832f177
Tamires Caires <tamiresdec@gmail.com>
2023-11-01 17:12:19 -0300

    fix: Correção de bug ao excluir contatos da agenda e update do CRUD

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -32,7 +32,7 @@ exports.editIndex = async (req, res) => {
   const contato = await Contato.buscaPorId(req.params.id);
   console.log(contato);
   
-  // if(!contato) return res.render('404');
+  if(!contato) return res.render('404');
   res.render('contato', { contato });
 };
 
@@ -56,6 +56,17 @@ exports.edit = async (req, res) =>{
     res.render('404');
   };
   
-}
+};
+
+exports.delete = async (req, res) =>{
+  if(!req.params.id) return res.render('404');
+
+  const contato = await Contato.delete(req.params.id);
+  if(!contato) return res.render('404');
+
+  req.flash('success', 'Contato apagado com sucesso.');
+  req.session.save(() => res.redirect('back') );
+  return;
+};
 
 
--- src/models/ContatoModel.js
+++ src/models/ContatoModel.js
@@ -17,12 +17,6 @@ function Contato(body) {
   this.contato = null;
 }
 
-Contato.buscaPorId = async function(id){
-  if(typeof id !== 'string') return;
-  const user = await ContatoModel.findById(id);
-  return user;
-};
-
 Contato.prototype.register = async function(){
   this.valida();
 
@@ -59,6 +53,28 @@ Contato.prototype.edit = async function(id) {
   this.valida();
   if(this.errors.length > 0) return;
   this.contato = await ContatoModel.findByIdAndUpdate(id, this.body, { new: true })
-}
+};
+
+
+// Static Methods 
+Contato.buscaPorId = async function(id){
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findById(id);
+  return contato;
+};
+
+Contato.buscaContatos = async function(){
+  const contatos = await ContatoModel.find()
+    .sort({ criadoEm: -1 });
+  return contatos;
+};
+
+Contato.delete = async function(id) {
+  if(typeof id !== 'string') return;
+  const contato = await ContatoModel.findOneAndDelete({ _id: id });
+  return contato;
+};
+
+
 
 module.exports = Contato;
